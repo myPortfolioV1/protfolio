9e390e5482ea7eb6959e71078e30871533d6b99f
=aminejelass <=aminejelassi95@gmail.com>
2022-03-23 19:27:49 +0100

    my portfolio base code

---
+++ src/portfolio.js
@@ -0,0 +1,87 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  title: 'My portfolio.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Med Amine Jelassi',
+  role: 'full stack developper',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React', 'redux' ] ,
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React', 'redux' ] ,
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+ "javascript",
+ "react",
+ "react Native",
+ "redux",
+ "angular",
+ "vue js",
+ "node",
+ "express",
+ "sql",
+ "MySQL",
+ "oracle",
+ "postgresQL"
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'johnsmith@mail.com',
+}
+
+export { header, about, projects, skills, contact }
