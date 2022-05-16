c11b06bd0691220a13fb618e68f3e0512a2b07fe
=aminejelass <=aminejelassi95@gmail.com>
2022-05-16 07:56:54 +0100

    projects component refactoring

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,11 +10,24 @@ const Projects = () => {
     <section id='NEFFEX ' className='section projects'>
       <h2 className='section__title'>Projects</h2>
 
-      <div className='projects__grid'>
+      {/* <div className='projects__grid'>
         {projects.map((project) => (
           <ProjectContainer key={uniqid()} project={project} />
         ))}
-      </div>
+      </div> */}
+
+     <div className="projects_List_Container">
+          <div className="project_item_Container">
+
+          </div>
+
+          <div className="projects_List">
+
+          </div>
+
+     </div>
+
+
     </section>
   )
 }
